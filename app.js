945e6d81fbef4b64109121a572b4e70436c2a881
ibrahim <127866141+ibrahimalsalim@users.noreply.github.com>
2024-06-23 01:22:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const { notFound, errorHanlder } = require("./middlewares/errors")
 const logger = require("./middlewares/logger")
-const cors = require("cors");
+
 // const helmet = require("helmet");
 require("dotenv").config()
 
