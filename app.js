d99e35c43c96ed2bdfe8557a243c1ddfb1164803
ibrahim <127866141+ibrahimalsalim@users.noreply.github.com>
2024-06-23 02:34:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.use(errorHanlder);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on porttttttttttttt ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
